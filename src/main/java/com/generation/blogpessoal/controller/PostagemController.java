e1feef2ce42137b74c695ddf51ea8fc50548465e
unknown <ellenmayarap@gmail.com>
2024-05-22 13:48:00 -0300

    Métodos do blog pessoal

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,27 +1,70 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 
-@RestController // anotação que diz para spring que essa é uma conroladora de totas de acesso aos métodes
+import jakarta.validation.Valid;
+
+@RestController // anotação que diz para spring que essa é uma conroladora de totas de acesso
+				// aos métodes
 @RequestMapping("/postagens") // rota para chegar nessa classe "insomnia"
 @CrossOrigin(origins = "*", allowedHeaders = "*") // liberar o acesso a outras máquinas
 public class PostagemController {
 	@Autowired // injeção de dependencia - instanciar a classe PostagemRepository
 	private PostagemRepository postagemRepository;
-	
-	@GetMapping //
-	public ResponseEntity<List<Postagem>> getAll(){
+
+	@GetMapping // define o verbo http que atende esse metodo
+	public ResponseEntity<List<Postagem>> getAll() {
+		// ResponseEntity = classe
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+		// findById = SELECT * FROM tb_postagens WHERE id = 1;
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	//SELECT * FROM tb_postagens WHERE titulo = "titulo";
+		@GetMapping("/titulo/{titulo}")
+		public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+			return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+		}
+		//INSERT INTO tb_postagens (titulo, texto, data) VALUES ("Título", "Texto", "2024-12-31 14:05:01");
+		@PostMapping
+		public ResponseEntity<Postagem>post(@Valid @RequestBody Postagem postagem){
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(postagemRepository.save(postagem));
+		}
+		@PutMapping
+		public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+			return postagemRepository.findById(postagem.getId())
+					.map(resposta-> ResponseEntity.status(HttpStatus.OK)
+					.body(postagemRepository.save(postagem)))
+					.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		}
+		@DeleteMapping("/{id}")
+		public void delete(@PathVariable Long id) {
+			Optional<Postagem> postagem = postagemRepository.findById(id);
+			if(postagem.isEmpty())
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+			postagemRepository.deleteById(id);
+		}
 }
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,11 +1,15 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
 //JPA assume a responsabilidade de criar automaticamente o método de criar query no banco de dados
 //JpaReporitoy - classe JPA - metodos que vão realizar query no banco de dados
 public interface PostagemRepository extends JpaRepository <Postagem, Long >{
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
 
 }
