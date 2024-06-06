74f010c1745c585ab177b2e8b7ec898b515c5531
unknown <ellenmayarap@gmail.com>
2024-06-05 23:13:53 -0300

    implementando service e security

---
+++ src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -0,0 +1,78 @@
+package com.generation.blogpessoal.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class BasicSecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter authFilter; //Filtro JWT para autenticação de tokens
+    //ajustes usuario e senha - Define o serviço que carregará os detalhes do usuário a partir do banco de dados
+    @Bean
+    UserDetailsService userDetailsService() {
+
+        return new UserDetailsServiceImpl();
+    }
+    //criptografia da senha PasswordEncoder
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    //conseguiu validar usuario e senha
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+    //implementar gerenciamento e autenticação
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    //Configura a cadeia de filtros de segurança
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    	//Define as regras de autorização para as requisições HTTP
+    	http
+	        .sessionManagement(management -> management
+	                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+	        		.csrf(csrf -> csrf.disable())
+	        		.cors(withDefaults());
+
+    	http
+	        .authorizeHttpRequests((auth) -> auth
+	                .requestMatchers("/usuarios/logar").permitAll() //Permite acesso público ao endpoint de login
+	                .requestMatchers("/usuarios/cadastrar").permitAll() //Permite acesso público ao endpoint de cadastro
+	                .requestMatchers("/error/**").permitAll() //Permite acesso público aos endpoints de erro
+	                .requestMatchers(HttpMethod.OPTIONS).permitAll() //Permite todas as requisições OPTIONS
+	                .anyRequest().authenticated()) //Requer autenticação para qualquer outra requisição
+	        .authenticationProvider(authenticationProvider()) //Configura o provedor de autenticação
+	        .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+	        .httpBasic(withDefaults());
+
+		return http.build();
+
+    }
+
+}
---
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -0,0 +1,87 @@
+package com.generation.blogpessoal.security;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+ //objetivo da classe criar e confirmar o token
+ 
+@Component
+public class JwtService {
+	
+	//site para acessarmos a secret https://www.keygen.io/ opção sha 256
+	//constante que gera uma chave para encodar as informações do token
+	public static final String SECRET = "a4ba1c90acd1e54e8b500bd32f2432ae88afad7de3df44d162eaa69226c70c9f";
+	
+	//assinatura token
+	private Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+	
+	//método que tem como objeto retornar todas as claims inseridas
+	private Claims extractAllClaims(String token) {
+		return Jwts.parserBuilder()
+				.setSigningKey(getSignKey()).build()
+				.parseClaimsJws(token).getBody();
+	}
+	    
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+		
+	//recuperar os dados da parte sub do claim onde encontramos o email(usuario)
+	
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	//data que o token expira
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+	//valida se a data que o token expira esta dentro da validade
+	private Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+	
+	//validar se o usuario que foi extraido o token condiz com o usuario que a userdetails tem e se está dentro da dara de validade
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+	
+	//objetivo de calcular o tempo de validade do token, formar o claim com as informações do token
+		
+	 	private String createToken(Map<String, Object> claims, String userName) {
+		return Jwts.builder()
+					.setClaims(claims)
+					.setSubject(userName)
+					.setIssuedAt(new Date(System.currentTimeMillis()))
+					.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+					.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+	}
+
+	public String generateToken(String userName) {
+		Map<String, Object> claims = new HashMap<>();
+		return createToken(claims, userName);
+	}
+		
+	//gerar o token puxando os claims formados no metodo anterior
+	public String generateToken(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
